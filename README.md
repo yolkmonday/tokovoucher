f58475b819155d13458797a80ee5c7e4032ca6fc
aripadrian <aripadrian01@gmail.com>
2022-09-20 09:47:30 +0700

    ⚡️ init

---
+++ README.md
@@ -0,0 +1,78 @@
+[![CodeFactor](https://www.codefactor.io/repository/github/aripadrian/apigames/badge)](https://www.codefactor.io/repository/github/aripadrian/apigames)
+# Apigames Client for Node Js
+
+This library is the abstraction of Apigames API for access from applications written with server-side Javascript.
+
+[![NPM](https://nodei.co/npm/apigames.png)](https://nodei.co/npm/apigames/)
+
+
+## Instalasi
+
+```bash
+npm install apigames
+```
+
+atau
+
+```bash
+yarn add apigames
+```
+
+## Pemakaian
+Dapatkan Merchant ID dan Secret Key Anda di [Apigames Dashboard](https://member.apigames.id/pengaturan/secret-key).
+
+```js
+const Apigames = require('apigames');
+const client = new Apigames("YOUR MERCHANT ID","YOUR SECRET");
+```
+
+
+### Cek Saldo
+```js
+let saldo = await client.cekSaldo();
+```
+
+### Cek Akun Game
+```js
+Coming Soon
+```
+
+### Cek Status Koneksi
+```js
+Coming Soon
+```
+
+### Transaksi
+
+```js
+let transaksi = await client.transaksi(productCode, tujuan, refId);
+```
+
+> Note:
+> RefID adalah kode transaksi unik kamu yang di generate secara acak
+
+### Cek Status Transaksi
+```js
+Coming Soon
+```
+
+### Radeem Kiosgamer Garena Shell Bulk
+```js
+Coming Soon
+```
+
+### Cek Status Radeem Kiosgamer Garena Shell Bulk
+```js
+Coming Soon
+```
+
+### License
+
+[MIT](https://github.com/aripadrian/apigames/blob/master/LICENSE)
+
+### Author
+
+[Ari Padrian](mailto:aripadrian@gmail.com)
+
+
+
---
+++ index.js
@@ -0,0 +1,67 @@
+const crypto = require("crypto")
+const rp = require("request-promise-native");
+
+class Tokovoucher {
+  /**
+   * @param {string} merchant - Merchant ID
+   * @param {string} secret - Secret Key
+   **/
+  constructor(merchant, secret) {
+    this._merchant = merchant;
+    this._secret = secret;
+    this._endpoint = "https://api.tokovoucher.id"
+  }
+
+  cekSaldo() {
+    let signature = crypto
+      .createHash('md5')
+      .update(`${this._merchant}${this._secret}`)
+      .digest('hex')
+
+    const options = {
+      method: "GET",
+      uri: `${this._endpoint}/merchant/${this._merchant}?signature=${signature}`,
+      json: true,
+    };
+
+    return rp(options)
+      .then(function (resp) {
+        if (resp.data) {
+          if (typeof resp.data.saldo !== undefined) {
+            return resp.data.saldo;
+          } else {
+            throw Error(resp.data.error_msg);
+          }
+        }
+      })
+      .catch(function (err) {
+        throw Error(err);
+      });
+  }
+
+
+  /**
+   * @param {string} productCode - Kode Produk
+   * @param {string} tujuan - Tujuan Pengisian
+   * @param {string} refId - Ref ID Unik Anda
+   **/
+  transaksi(productCode, tujuan, refId) {
+    const options = {
+      method: 'GET',
+      uri: `${this._endpoint}/transaksi/http-get-v1?merchant=${this._merchant}&secret=${this._secret}&produk=${productCode}&tujuan=${tujuan}&ref=${refId}`,
+      json: true,
+    };
+
+    return rp(options)
+      .then(function (resp) {
+        if (resp.data) {
+          return resp.data;
+        }
+      })
+      .catch(function (err) {
+        throw Error(err);
+      });
+  }
+}
+
+module.exports = Apigames;
